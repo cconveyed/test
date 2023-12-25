29b15829c183017705dfaef57f0148825e2b107f
cconveyed <yuvanv07@gmail.com>
2023-12-25 17:16:43 +0000

    g

--- g.py
+++ g.py
@@ -4,3 +4,6 @@ from bs4 import BeautifulSoup
 
 gees = {'money':543,
         'power':4}
+url='https://libraryofbabel.info/book.cgi?1-w4-s1-v20:1'
+website = requests.get(url).text
+doc=BeautifulSoup(website,'html.parser')
